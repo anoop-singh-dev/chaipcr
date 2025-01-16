c00019eddbc636844310ebb027d3d1badc7b4165
Mahammad <cairo74@gmail.com>
2025-01-16 04:12:07 +0000

    fix save and ruby, and deploy jobs

--- devops/jenkins/update_repo.sh
+++ devops/jenkins/update_repo.sh
@@ -4,14 +4,9 @@ cp --parents  /var/lib/jenkins/jobs/*/config.xml -r .
 cp -r var/lib/jenkins/jobs .
 rm -r var
 
-find . -name *.xml -type f -exec sed -i "s/192.168.1.201/10.0.1.20/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.200/10.0.1.20/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.33/10.0.100.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.44/10.0.100.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.55/10.0.100.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.112/10.0.100.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.88/10.0.100.240/g"  {} \;
-find . -name *.xml -type f -exec sed -i "s/192.168.1.99/10.0.100.240/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.81.39/192.168.68.110/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.81.81/192.168.68.190/g"  {} \;
+find . -name *.xml -type f -exec sed -i "s/192.168.81.32/192.168.68.191/g"  {} \;
 
 sync
 
